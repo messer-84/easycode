238d459457e5392a8050a92008b52dc9eb1fc249
messer <gtl.vorobyov@gmail.com>
2017-05-12 23:27:19 +0300

    add

---
+++ lesson_1/homework_1.js
@@ -0,0 +1,11 @@
+let contact = {
+  name: 'Ivan',
+  age: '32',
+  phoneNumbers: ['0502225566', '0972225566', '0632225566'],
+  socialLinks: {
+    vk: 'vklink.com',
+    facebook: 'facebooklink.com',
+    twitter: 'twitterlink.com'
+  },
+  isFavorite: true
+};
