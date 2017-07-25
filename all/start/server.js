6cb24cb4d965e2dfca9b4f94fbe4c86210096852
maksim <gtl.vorobyov@gmail.com>
2017-07-25 18:25:05 +0300

    hw19

---
+++ all/start/server.js
@@ -0,0 +1,14 @@
+var User = require('./user');
+
+function run() {
+  var vasya = new User('Vasya');
+  var petya = new User('Petya');
+
+  vasya.hello(petya);
+}
+
+if(module.parent){
+  exports.run = run;
+} else{
+  run();
+}
